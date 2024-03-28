d5660a2f88d6c2f26bc6713635c987251f33242b
Kevin Stefanov <kevinstefanov15@gmail.com>
2024-03-28 21:05:04 +0200

    Work on client graphica user interface.

--- src/client_GUI.c
+++ src/client_GUI.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <string.h>
 
 static void my_func (GtkWidget *widget, gpointer data){
 	g_print ("Hello World\n");
@@ -9,101 +10,134 @@ static void activate (GtkApplication *app, gpointer user_data){
 
 	const gchar  *label_btn_reg = "Register"
 				,*label_btn_log = "Login"
-	            ,*label_hlo_txt = "Welcome to Rosetta Secure Texting"
-	            ,*label_hello_CSSname = "CSS_label_hello"
+	            ,*label_ROSETTA_CSSname = "CSS_label_ROSETTA"
 	            ; 
-	           
+
 	GtkWidget *window
-			 ,*box_main
-			 ,*box_left,    *frame_box_left
-			 ,*box_mid,     *frame_box_mid
-			 ,*box_right,   *frame_box_right
-			 ,*box_welc,    *frame_box_welc
-			 ,*box_logreg,  *frame_box_logreg
-			 ,*label_hello
+			 ,*grid_main
+			 ,*label_empty
+			 ,*label_empty2
+			 ,*label_empty3
+			 ,*label_ROSETTA
 			 ,*btn_log
 			 ,*btn_reg
 			 ;
+
 
 	window = gtk_application_window_new (app);
 	gtk_window_set_title (GTK_WINDOW (window), "Rosetta");
 	gtk_window_set_default_size (GTK_WINDOW (window), 1920, 1080);
 
 
-	box_main = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-	gtk_box_set_homogeneous(GTK_BOX(box_main), TRUE);
+	grid_main = gtk_grid_new();
 	
-	gtk_window_set_child (GTK_WINDOW (window), box_main);
 
 
-	box_left  = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	gtk_box_set_homogeneous(GTK_BOX(box_left), TRUE);
-	
-	box_mid   = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	gtk_box_set_homogeneous(GTK_BOX(box_mid), TRUE);
-	
-	box_right = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	gtk_box_set_homogeneous(GTK_BOX(box_right), TRUE);
-	
-	gtk_box_append(GTK_BOX(box_main), box_left);
-	gtk_box_append(GTK_BOX(box_main), box_mid);
-	gtk_box_append(GTK_BOX(box_main), box_right);
-	
-	
-	box_welc   = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	gtk_box_set_homogeneous(GTK_BOX(box_welc), TRUE);	
-	
-	box_logreg = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-	gtk_box_set_homogeneous(GTK_BOX(box_logreg), TRUE);
-	
-	gtk_box_append(GTK_BOX(box_mid), box_welc);
-	gtk_box_append(GTK_BOX(box_mid), box_logreg);
-	
-	
-	
-	
-	frame_box_left = gtk_frame_new(NULL);
-	gtk_box_append(GTK_BOX(box_left), frame_box_left);
-	
-	frame_box_mid = gtk_frame_new(NULL);
-	gtk_box_append(GTK_BOX(box_mid), frame_box_mid);
+	gtk_window_set_child (GTK_WINDOW (window), grid_main);
 
-	frame_box_right = gtk_frame_new(NULL);
-	gtk_box_append(GTK_BOX(box_right), frame_box_right);
+
+
+	label_empty  = gtk_label_new("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	label_empty2 = gtk_label_new("\n");
+	label_empty3 = gtk_label_new("\n");
+	label_ROSETTA = gtk_label_new( 
+"                                            "                                                                                                                                                                                                           
+"RRRRRRRRRRRRRRRRR         OOOOOOOOO         SSSSSSSSSSSSSSS  EEEEEEEEEEEEEEEEEEEEEE TTTTTTTTTTTTTTTTTTTTTTT TTTTTTTTTTTTTTTTTTTTTTT          AAA\n"             
+"                                            "
+"R::::::::::::::::R      OO:::::::::OO     SS:::::::::::::::S E::::::::::::::::::::E T:::::::::::::::::::::T T:::::::::::::::::::::T         A:::A\n"              
+"                                            "
+"R::::::RRRRRR:::::R   OO:::::::::::::OO  S:::::SSSSSS::::::S E::::::::::::::::::::E T:::::::::::::::::::::T T:::::::::::::::::::::T        A:::::A\n"             
+"                                            "
+"RR:::::R     R:::::R O:::::::OOO:::::::O S:::::S     SSSSSSS EE::::::EEEEEEEEE::::E T:::::TT:::::::TT:::::T T:::::TT:::::::TT:::::T       A:::::::A\n"            
+"                                            "
+"  R::::R     R:::::R O::::::O   O::::::O S:::::S               E:::::E       EEEEEE TTTTTT  T:::::T  TTTTTT TTTTTT  T:::::T  TTTTTT      A:::::::::A\n"           
+"                                            "
+"  R::::R     R:::::R O:::::O     O:::::O S:::::S               E:::::E                      T:::::T                 T:::::T             A:::::A:::::A\n"          
+"                                            "
+"  R::::RRRRRR:::::R  O:::::O     O:::::O  S::::SSSS            E::::::EEEEEEEEEE            T:::::T                 T:::::T            A:::::A A:::::A\n"         
+"                                            "
+"  R:::::::::::::RR   O:::::O     O:::::O   SS::::::SSSSS       E:::::::::::::::E            T:::::T                 T:::::T           A:::::A   A:::::A\n"        
+"                                            "
+"  R::::RRRRRR:::::R  O:::::O     O:::::O     SSS::::::::SS     E:::::::::::::::E            T:::::T                 T:::::T          A:::::A     A:::::A\n"       
+"                                            "
+"  R::::R     R:::::R O:::::O     O:::::O        SSSSSS::::S    E::::::EEEEEEEEEE            T:::::T                 T:::::T         A:::::AAAAAAAAA:::::A\n"      
+"                                            "
+"  R::::R     R:::::R O:::::O     O:::::O             S:::::S   E:::::E                      T:::::T                 T:::::T        A:::::::::::::::::::::A\n"     
+"                                            "
+"  R::::R     R:::::R O::::::O   O::::::O             S:::::S   E:::::E       EEEEEE         T:::::T                 T:::::T       A:::::AAAAAAAAAAAAA:::::A\n"    
+"                                            "
+"RR:::::R     R:::::R O:::::::OOO:::::::O SSSSSSS     S:::::S EE::::::EEEEEEEE:::::E       TT:::::::TT             TT:::::::TT    A:::::A             A:::::A\n"   
+"                                            "
+"R::::::R     R:::::R  OO:::::::::::::OO  S::::::SSSSSS:::::S E::::::::::::::::::::E       T:::::::::T             T:::::::::T   A:::::A               A:::::A\n"  
+"                                            "
+"R::::::R     R:::::R    OO:::::::::OO    S:::::::::::::::SS  E::::::::::::::::::::E       T:::::::::T             T:::::::::T  A:::::A                 A:::::A\n" 
+"                                            "
+"RRRRRRRR     RRRRRRR      OOOOOOOOO       SSSSSSSSSSSSSSS    EEEEEEEEEEEEEEEEEEEEEE       TTTTTTTTTTT             TTTTTTTTTTT AAAAAAA                   AAAAAAA\n"
+); 
+	gtk_grid_attach(GTK_GRID(grid_main), label_empty3,   0, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid_main), label_ROSETTA, 0, 1, 16, 1);
+	gtk_grid_attach(GTK_GRID(grid_main), label_empty,   0, 2, 1, 1);
 	
-	frame_box_welc = gtk_frame_new(NULL);
-	gtk_box_append(GTK_BOX(box_welc), frame_box_welc);
-	
-	frame_box_logreg = gtk_frame_new(NULL);
-	gtk_box_append(GTK_BOX(box_logreg), frame_box_logreg);
 
 	
+	/*--------------------------------------------------------*/
 	
-	
-	label_hello = gtk_label_new(label_hlo_txt);
-	gtk_box_append(GTK_BOX(box_welc), label_hello);
-	
+	/* Define Register and Login buttons. */
 	btn_log = gtk_button_new_with_label(label_btn_log);
 	btn_reg = gtk_button_new_with_label(label_btn_reg);
-	
+
 	/*
 	g_signal_connect (button, "clicked", G_CALLBACK (my_func), NULL);
 	*/
+
+	/* Put the 2 buttons inside the main grid */
+
 	
-	gtk_box_append(GTK_BOX(box_logreg), btn_reg);
-	gtk_box_append(GTK_BOX(box_logreg), btn_log);
+	gtk_grid_attach (
+					  GTK_GRID(grid_main),
+					  btn_log,
+					  9, /* column */
+					  4,  /* row    */
+					  1,  /* width  */
+					  1   /* height */
+					);
+	
+	gtk_grid_attach(GTK_GRID(grid_main), label_empty2,   9, 5, 1, 1);
+	
+	gtk_grid_attach (
+					  GTK_GRID(grid_main),
+					  btn_reg,
+					  9, /* column */
+					  6,  /* row    */
+					  1,  /* width  */
+					  1   /* height */
+					);
+	
+    //gtk_grid_set_column_homogeneous( GTK_GRID(grid_main), TRUE);
+    //gtk_grid_set_row_homogeneous(    GTK_GRID(grid_main), TRUE);
+
+	/* Add GTK-CSS styling to make the borders actually visible and red. */
 	
 	/* Give it a CSS-recognizable name. */
-	gtk_widget_set_name(label_hello, label_hello_CSSname);
+	gtk_widget_set_name(label_ROSETTA, label_ROSETTA_CSSname);
 	
 	/* Load the CSS file into this GTK app */
 	GtkCssProvider *cssProvider = gtk_css_provider_new();
 	
 	gtk_css_provider_load_from_data(
 		cssProvider,
-		"frame{border:10px solid red;} "
-		"label#CSS_label_hello{font: 30px 'Comic Sans';}\0"
-		,78
+		"frame{border:14px solid red;}"
+	    "label#CSS_label_ROSETTA{font:14px;}"
+	    "label{font-family:monospace; white-space:pre;}"
+	    "button{font-family:monospace; white-space:pre;}"
+	    "box{font-family:monospace; white-space:pre;}\0"
+		,strlen(
+				"frame{border:14px solid red;}"
+	    "label#CSS_label_ROSETTA{font:14px;}"
+	    "label{font-family:monospace; white-space:pre;}"
+	    "button{font-family:monospace; white-space:pre;}"
+	    "box{font-family:monospace; white-space:pre;}\0"
+		)
 	);
 	
     gtk_style_context_add_provider_for_display(  gdk_display_get_default()
