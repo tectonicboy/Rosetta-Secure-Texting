23388de290faaeabb56b0723a180d48165a1f86e
tectonicboy <kevinstefanov15@gmail.com>
2025-07-27 21:56:55 +0300

    Continue working on Rosetta Test Framework.

--- src/rosetta-test-framework/test-control.c
+++ src/rosetta-test-framework/test-control.c
@@ -26,6 +26,9 @@ void draw_menu_0(){
     int bytes;
     FILE* logo_file = fopen("rtf-logo.txt", "r");
     unsigned char* logo_buf;    
+    unsigned int   op_number;
+
+    /* ========================= PART 1: Draw logo ========================== */
 
     fseek(logo_file, 0, SEEK_END);  
 
@@ -39,7 +42,73 @@ void draw_menu_0(){
 
     logo_buf[bytes] = '\0';
 
-    printf("\n\n\n%s\n\n\n", logo_buf);
+    printf("\n\n\n%s\n\n\n\n\n\n\n\n\n\n", logo_buf);
+
+    /* ====================== PART 2: Top menu options ====================== */
+
+    printf("The following control console operations are supported by RTF.\n");
+    printf("Enter an option number to perform its test operation.\n\n\n");
+
+    printf(
+     "#========#============================================================#\n"
+     "| OPTION | Operation performed                                        |\n"
+     "#========#============================================================#\n"
+     "|      1 | Create a new test user account at a custom directory.      |\n"
+     "|      2 | Start a test user chat session (login).                    |\n"
+     "|      3 | Stop  a test user chat session (logout)                    |\n"
+     "|      4 | Delete a test user account.                                |\n"
+     "|      5 | Start the Rosetta server OS process, for AF_UNIX ipc.      |\n"
+     "|      6 | Stop  the Rosetta server.                                  |\n"
+     "|      7 | Close all testing and exit RTF.                            |\n"
+     "#========#============================================================#\n"
+     "\n"
+    );
+
+label_again:
+
+    printf("\nEnter next operation: ");
+
+    scanf("%u", &op_number);
+
+    printf("\n\nOperation selected: ");
+
+    if(op_number == 1){
+        printf("Creating a new test user account.\n");
+        printf("[NOT IMPLEMENTED YET]\n\n");
+    }
+    else if(op_number == 2){
+        printf("Starting a test user chat session (login).\n");
+        printf("[NOT IMPLEMENTED YET]\n\n");
+    }
+    else if(op_number == 3){
+        printf("Stopping a test user chat session (logout).\n");
+        printf("[NOT IMPLEMENTED YET]\n\n");
+    }
+    else if(op_number == 4){
+        printf("Deleting a test user account.\n");
+        printf("[NOT IMPLEMENTED YET]\n\n");
+    }
+    else if(op_number == 5){
+        printf("Starting the Rosetta server with AF_UNIX communications.\n");
+        printf("[NOT IMPLEMENTED YET]\n\n");
+    }
+    else if(op_number == 6){
+        printf("Stopping the Rosetta server.\n");
+        printf("[NOT IMPLEMENTED YET]\n\n");
+    }
+    else if(op_number == 7){
+        printf("Closing all testing and exiting Rosetta Test Framework.\n");
+        goto label_cleanup;
+    }
+    else{
+        printf( "\n< %u is not a supported operation number. Try again...\n\n"
+               ,op_number
+        );
+    }
+
+    goto label_again;
+
+label_cleanup:
 
     free(logo_buf);
     fclose(logo_file);
@@ -51,4 +120,5 @@ int main(){
 
     draw_menu_0();
 
+    return 0;
 }
