320ced2e2dc40e26d26ee293d3c03f263b7a9f9f
Kevin Stefanov <kstefanov@mm-sol.com>
2025-09-10 11:33:18 +0300

    test from new PC

--- src/lib/bigint.h
+++ src/lib/bigint.h
@@ -6,6 +6,8 @@
 #include <stdbool.h>
 #include <math.h>
 
+/******************************************************************************/
+
 #define u8  uint8_t
 #define u16 uint16_t
 #define u32 uint32_t
