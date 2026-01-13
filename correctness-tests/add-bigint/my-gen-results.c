887069b108a03bae0c8f04c1ba31f28556499077
tectonicboy <kevinstefanov15@gmail.com>
2026-01-13 15:02:54 +0200

    [3] Correctness tests: First successful test passed!

--- correctness-tests/add-bigint/my-gen-results.c
+++ correctness-tests/add-bigint/my-gen-results.c
@@ -2,13 +2,12 @@
 
 #define TEST_MAX_BYTES 512
 
-void generate_and_add_large_ints(bigint*  initial_kick, 
-                                 uint64_t count, 
-                                 uint64_t nr_operand_bits)
+void generate_and_add_large_ints(bigint* __restrict__ initial_kick, 
+                                 const uint64_t count)
 {
     char filename[128];
     strcpy(filename, "../calc-results/add-test-0-result-my-0.dat\0");
-    FILE* results_fd = NULL;
+    FILE* __restrict__ results_fd = NULL;
 
     bigint operand1;
     bigint operand2;
@@ -32,16 +31,12 @@ void generate_and_add_large_ints(bigint*  initial_kick,
     bigint_create(&temp2,        TEST_MAX_BYTES * 8, 0);
     bigint_create(&one,          TEST_MAX_BYTES * 8, 1);
 
-    printf("[DEBUG] strlen(filename) = %lu\n", strlen(filename));
-
     if( system("rm -f ../calc-results/add-test-0-result-my-*") < 0){
         printf("[ERR] Could not delete existing C code result DAT files.\n");
         exit(1);
     }
    
     for(uint64_t i = 0; i < count;  ++i){
-        bigint_add_fast(&i_big, &one, &temp1);
-        bigint_equate2(&i_big, &temp1);
         bigint_pow(&i_big, &ninety_big, &temp1);
 
         bigint_mul_fast(&temp1, &temp_op1_mul, &temp2);
@@ -53,32 +48,19 @@ void generate_and_add_large_ints(bigint*  initial_kick,
         /* The big final addition to produce test results. */
         bigint_add_fast(&operand1, &operand2, &result);
 
-        /* Store in a file for checking with python's builtin bigint engine. */
-        /* Retarded way to do it but will do for now. */
-        if( i > 0 && (i % (count/10) == 0) ){
-            printf("Reached %lu results! New file DAT starting.\n", i);
-            if(i / (count/10) == 1.0) { filename[strlen(filename) - 5] = '1'; }
-            if(i / (count/10) == 2.0) { filename[strlen(filename) - 5] = '2'; }
-            if(i / (count/10) == 3.0) { filename[strlen(filename) - 5] = '3'; }
-            if(i / (count/10) == 4.0) { filename[strlen(filename) - 5] = '4'; }
-            if(i / (count/10) == 5.0) { filename[strlen(filename) - 5] = '5'; }
-            if(i / (count/10) == 6.0) { filename[strlen(filename) - 5] = '6'; }
-            if(i / (count/10) == 7.0) { filename[strlen(filename) - 5] = '7'; }
-            if(i / (count/10) == 8.0) { filename[strlen(filename) - 5] = '8'; }
-            if(i / (count/10) == 9.0) { filename[strlen(filename) - 5] = '9'; }
-        }
-        
-        results_fd = fopen(filename, "a");
-        if(results_fd == NULL){
+        if(__builtin_expect((results_fd = fopen(filename, "a")) == NULL, 0)){
             perror("[ERR] fopen() failed. errno: ");
             exit(1);
         }
-        if( fwrite(result.bits, 1, 405, results_fd) != 405){
+        if(__builtin_expect(fwrite(result.bits, 1, 405, results_fd) != 405, 0)){
             perror("[ERR] fwrite() failed. errno: ");
             exit(1);
         }
         
         fclose(results_fd);
+
+        bigint_add_fast(&i_big, &one, &temp1);                                   
+        bigint_equate2(&i_big, &temp1); 
     }
 
     return;
@@ -88,8 +70,7 @@ int main(){
     bigint start_bigint;
     bigint multiplier;
     bigint temp_mul_result;
-    uint64_t operand_bits = 32;
-    uint64_t nr_add_results = 10000000;
+    const uint64_t nr_add_results = 10000000;
 
     bigint_create(&start_bigint,    TEST_MAX_BYTES * 8, 0xFF000000);
     bigint_create(&multiplier,      TEST_MAX_BYTES * 8, 0xFF000000);
@@ -101,12 +82,13 @@ int main(){
     for(uint64_t i = 0; i < 100; ++i){
         bigint_mul_fast(&start_bigint, &multiplier, &temp_mul_result);
         bigint_equate2(&start_bigint, &temp_mul_result);
-        operand_bits += 32;
     }
 
     printf("Computed the initial kick number! Results calculation startingg\n");
 
-    generate_and_add_large_ints(&start_bigint, nr_add_results, operand_bits);
+    generate_and_add_large_ints(&start_bigint, nr_add_results);
     
+    printf("BigInt ADD Test 1 - Own engine finished generating results.\n");
+
     return 0;
 }
--- correctness-tests/add-bigint/py-gen-results.py
+++ correctness-tests/add-bigint/py-gen-results.py
@@ -1,13 +1,9 @@
 import os
 
 def generate_and_add_large_ints(initial_kick, count, nr_operand_bits):
-    
-    # Calculate how many bytes are needed to store the result
-    # Addition can produce a result with 1 more bit than the bigger operand's.
-    # In this case both operands have the same bitlength.
+
     result_bytelen = nr_operand_bits + 1
-    base_filename_str = "../calc-results/add-test-0-result-py-"
-    base_filename_ext = ".dat"
+    full_filename_str = "../calc-results/add-test-0-result-py.dat"
 
     while result_bytelen % 8 != 0:
         result_bytelen += 1
@@ -21,7 +17,6 @@ def generate_and_add_large_ints(initial_kick, count, nr_operand_bits):
         operands_1[i] = initial_kick + (17_000_000 * pow(i, 90))
         operands_2[i] = initial_kick + (12_111_222 * pow(i, 90))
 
-    full_filename_str = f"{base_filename_str}{0}{base_filename_ext}"
     os.makedirs(os.path.dirname(full_filename_str), exist_ok=True)
     with open(full_filename_str, 'wb') as f:
         f.truncate()
@@ -29,27 +24,8 @@ def generate_and_add_large_ints(initial_kick, count, nr_operand_bits):
     for i in range(count):
         results[i] = operands_1[i] + operands_2[i]
         raw_bytes = results[i].to_bytes(result_bytelen, byteorder='little')
-
-        # Track the progress and change file to write results to occasionally.
-        if i % 1000000 == 0 and i > 0:
-            batch = i//1000000
-            full_filename_str = f"{base_filename_str}{batch}{base_filename_ext}"
-            os.makedirs(os.path.dirname(full_filename_str), exist_ok=True)
-            with open(full_filename_str, 'wb') as f:
-                f.truncate()
-            print(f"Batch {batch} with 1,000,000 bigint python ADDs done!")
-            print(f"New file for results starting: {full_filename_str}")
-
         with open(full_filename_str, 'ab') as f:
             f.write(raw_bytes)
-
-        if i == 0:
-            with open('./FIRST-NUM.dat','wb') as f:
-                f.write(raw_bytes)
-
-        if i == count - 1:
-            with open('./LAST-NUM.dat','wb') as f:
-                f.write(raw_bytes)
 
     print("Done. Each python bigint ADD result is in DAT files.")
     print(f"Each number is stored in {result_bytelen} bytes.")
